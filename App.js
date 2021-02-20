6d86ea2d4472948d37936fba50adba59447594e7
Bichurina Anastasiia <69593629+bratzzie@users.noreply.github.com>
2021-02-20 13:59:12 +0200

    added component of a car

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import CarItem from './components/CardItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+     <CarItem />
+    
+    
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -18,4 +22,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
